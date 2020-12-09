cc63e6406e3502963e5e8ac80419afc2fce30916
garciaveravictor <71645624+garciaveravictor@users.noreply.github.com>
2020-12-09 17:16:29 +0100

    4.5.7

--- UIControlSamples/src/listviewsample/ListViewSample.java
+++ UIControlSamples/src/listviewsample/ListViewSample.java
@@ -43,7 +43,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -86,7 +86,7 @@ public class ListViewSample extends Application {
         @Override
         public void updateItem(String item, boolean empty) {
             super.updateItem(item, empty);
-            Rectangle rect = new Rectangle(100, 20);
+            Circle rect = new Circle( 20);
             if (item != null) {
                 rect.setFill(Color.web(item));
                 setGraphic(rect);
